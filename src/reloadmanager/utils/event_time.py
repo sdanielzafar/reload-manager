e2dc947f1cb17752b8d12cd08912e55ad2032560
dan.zafar <dan.zafar@databricks.com>
2025-04-27 17:31:27 -0600

    add validation functionality

--- src/reloadmanager/utils/event_time.py
+++ src/reloadmanager/utils/event_time.py
@@ -1,13 +1,6 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-# def validate_dt_fmt(dt_string: str) -> str:
-#     try:
-#         datetime.strptime(dt_string, '%Y-%m-%d %H:%M:%S')
-#         return dt_string
-#     except ValueError:
-#         raise ValueError(f"Datetime passed {dt_string} is incorrect. Must match format '%Y-%m-%d %H:%M:%S'")
-
 
 class EventTime(int):
     __slots__ = ("_tz",)
@@ -20,12 +13,20 @@ class EventTime(int):
     def __new__(cls, ts_str: str | None):
         if ts_str is None:
             raise ValueError("None is not a valid EventTime")
+        cls._validate_dt_str(ts_str)
         dt = datetime.strptime(ts_str, "%Y-%m-%d %H:%M:%S")
         dt = dt.replace(tzinfo=cls._default_tz)
         epoch = int(dt.timestamp())
         obj = super().__new__(cls, epoch)
         obj._tz = cls._default_tz
         return obj
+
+    def _validate_dt_str(dt_string: str) -> str:
+        try:
+            datetime.strptime(dt_string, '%Y-%m-%d %H:%M:%S')
+            return dt_string
+        except ValueError:
+            raise ValueError(f"Datetime passed '{dt_string}' is incorrect. Must match format '%Y-%m-%d %H:%M:%S'")
 
     def __str__(self):
         return datetime.fromtimestamp(int(self), tz=self._tz).strftime("%Y-%m-%d %H:%M:%S")
