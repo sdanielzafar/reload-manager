7ef6a9ce52894ef91c7409df6dbd6ed31ac619c1
dan.zafar <dan.zafar@databricks.com>
2025-04-28 12:02:53 -0600

    fix logger

--- src/reloadmanager/mixins/logging_mixin.py
+++ src/reloadmanager/mixins/logging_mixin.py
@@ -2,24 +2,18 @@ import logging
 
 
 class LoggingMixin:
-    @property
-    def logger(self):
-        logger = logging.getLogger(self.__class__.__name__)
-        logger.setLevel(logging.getLogger().getEffectiveLevel())
+    _fmt = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 
-        if not logger.hasHandlers():
-            handler = logging.StreamHandler()
-            handler.setLevel(logging.getLogger().getEffectiveLevel())
-            formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-            handler.setFormatter(formatter)
-            logger.addHandler(handler)
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
 
-        logger.propagate = False
-        return logger
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(self._fmt)
+        self.logger.addHandler(stream_handler)
 
-    def set_logger_level(self, log_level):
-        level = getattr(logging, log_level.upper(), None)
+    def set_logger_level(self, lvl: str):
+        level = getattr(logging, lvl.upper(), None)
         if not isinstance(level, int):
-            raise ValueError(f"Invalid log level: {log_level}")
-
+            raise ValueError(f"Invalid log level: {lvl}")
         self.logger.setLevel(level)
